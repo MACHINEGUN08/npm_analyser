44216f3faf9c003a361a7c2e12aae1b675057ba0
MACHINEGUN08 <97286687+MACHINEGUN08@users.noreply.github.com>
2022-04-15 15:58:24 +0545

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,16 @@ class Test(unittest.TestCase):
 
         self.assertEqual(package_details.package_name, 'babel-core')
         self.assertEqual(package_details.license, 'MIT')
+        self.assertEqual(package_details.version, '8.7.0')
+        self.assertEqual(package_details.last_published, '15 hours ago')
+        self.assertEqual(package_details.number_of_dependents, '4830')
+        self.assertEqual(package_details.get_dev_dependencies, '2')
+        self.assertEqual(package_details.total_versions, '470')
+        self.assertEqual(package_details.unpacked_size, '8.75 MB')
+        self.assertEqual(package_details.weekly_downloads, '4,196,615')
+        self.assertEqual(package_details.total_files, '1821')
         
+       
 if __name__ == '__main__':
     unittest.main()
+    
